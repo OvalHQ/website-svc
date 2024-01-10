ccb464ae0104a75a896a77cd091ac1604d8cdcc4
Oluwadara Oloye <daraoloye99@gmail.com>
2024-01-10 18:42:46 +0100

    style: add background layer vector to about us hero section

--- application/src/screens/general/about-us/index.tsx
+++ application/src/screens/general/about-us/index.tsx
@@ -3,12 +3,34 @@
 import SimplifyOperations from "@/components/home/simplify-operations";
 import MainLayout from "@/components/layout/main";
 import { Heading, Pane, Text } from "evergreen-ui";
+import Image from "next/image";
 
 const AboutUsScreen = () => {
   return (
     <MainLayout>
-      <Pane paddingY="175px" backgroundColor="#E2FDFF">
-        <Pane width="70%" marginX="auto">
+      <Pane paddingY="175px" backgroundColor="#E2FDFF" position="relative" zIndex="2">
+        <Pane display="flex" justifyContent="space-between" opacity=".3" position="absolute" top={0} zIndex={-4} width="100%" height="100%">
+          <Pane>
+            <Image
+              style={{
+                transform: "rotate(180deg)",
+                height: "100%"
+              }}
+              alt="background-layer"
+              src={require("../../../../public/images/general/pattern-bg-1.png")}
+            />
+          </Pane>
+          <Pane>
+            <Image
+              style={{
+                height: "100%"
+              }}
+              alt="background-layer"
+              src={require("../../../../public/images/general/pattern-bg-1.png")}
+            />
+          </Pane>
+        </Pane>
+        <Pane width="70%" marginX="auto" zIndex="10">
           <p className="md:font-medium md:text-2xl md:mb-8">About Us</p>
           <h2 className="md:font-semibold md:w-[934px] md:text-[70px] leading-[1]">
             We are empowering the next generation of founders with the financial tools they need to scale their business globally.
