c8a76d0a3dd8252cb523e1f5b4c1391a446f706b
Yomikensei <yomi.ndu@icloud.com>
2024-01-31 17:29:32 +0000

    chore: add tailwind, styled-components config

--- application/next.config.js
+++ application/next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    compiler: {
+        styledComponents: true,
+    },
     // output: 'export',
     experimental: {
         missingSuspenseWithCSRBailout: false,
--- application/src/app/layout.tsx
+++ application/src/app/layout.tsx
@@ -1,29 +1,32 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import type {Metadata} from "next";
+import {Inter} from "next/font/google";
 import "./globals.css";
 import "../../public/fonts/fonts.css";
-import Script from "next/script";
 import CustomScript from "@/components/CustomScript";
+import StyledComponentsRegistry from '../lib/registry'
 
-const inter = Inter({ subsets: ["latin"] });
+
+const inter = Inter({subsets: ["latin"]});
 
 export const metadata: Metadata = {
-  title: "Graph",
-  description:
-    "Experience seamless access to corporate cards, multi-currency accounts, and global payouts, all in one account.",
+	title: "Graph",
+	description:
+		"Experience seamless access to corporate cards, multi-currency accounts, and global payouts, all in one account.",
 };
 
 export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
+									   children,
+								   }: {
+	children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+	return (
+		<html lang="en">
+		<body className={inter.className}>
+		<StyledComponentsRegistry>{children}</StyledComponentsRegistry>
+		</body>
 
-      {/* Scripts */}
-      <CustomScript />
-    </html>
-  );
+		{/* Scripts */}
+		<CustomScript/>
+		</html>
+	);
 }
