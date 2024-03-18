f399d0dd0403712ef6b7b7522197318ae86892d2
Obiofiong <johnobiofiong1@gmail.com>
2024-03-18 13:11:48 +0100

    style: more styling for blog

--- application/src/screens/general/blog/index.style.ts
+++ application/src/screens/general/blog/index.style.ts
@@ -19,6 +19,24 @@ const BlogScreenWrapper = styled(Pane)`
     a {
       text-decoration: underline;
     }
+    h3 {
+      font-size: 24px;
+      margin: 20px 0;
+      font-weight: 600;
+      font-family: Neue Haas Grotesk Display Pro !important;
+    }
+    ul {
+      display: flex;
+      flex-direction: column;
+      gap: 15px;
+      font-family: Neue Haas Grotesk Display Pro !important;
+      li {
+        list-style-type: disc;
+        margin-left: 15px;
+        font-size: 20px;
+        font-family: Neue Haas Grotesk Display Pro !important;
+      }
+    }
   }
 `;
 
